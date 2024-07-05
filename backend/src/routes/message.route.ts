c397c59cd233bbfb545455f043b0f13583aa243a
Maryna Kucher <maryna.dev.k@gmail.com>
2024-07-05 10:44:27 +0300

    Message route and controller are implemented, typo is fixed

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { protectRoute } from '../middleware/protectRoute.js';
+import { messageController } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
-router.get('/conversations', (req, res) => {
-  res.send('Conversation route');
-});
+router.get('/conversations', protectRoute, messageController.getUsersForSidebar);
+router.get('/:id', protectRoute, messageController.getMessages);
+router.post('/send/:id', protectRoute, messageController.sendMessage);
 
 export default router;
