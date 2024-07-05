3e91e2258f1541be73d6312c21fcac586d9f2e99
Maryna Kucher <maryna.dev.k@gmail.com>
2024-07-05 08:51:18 +0300

    Auth controller is completely implemented

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import cookieParser from 'cookie-parser';
+
 
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+app.use(cookieParser());
+app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { authController } from '../controllers/auth.controller';
+import { authController } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
+router.get('/me', protectRoute, authController.getMe);
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
